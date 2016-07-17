a80fc7aacf1e2218fe1b38b7f31e30d5d37509ea
Humberto Borba <humberos@gmail.com>
2016-07-17 01:08:44 -0300

    leo: Move omni custom configuration to common
    Depends on:
    https://gerrit.omnirom.org/#/c/18573
    
    Signed-off-by: Humberto Borba <humberos@gmail.com>
    Change-Id: I93431a98583f35fb4c919fe0d3f81fa71a63635a

--- omni_leo.mk
+++ omni_leo.mk
@@ -26,9 +26,6 @@ $(call inherit-product, device/sony/leo/aosp_d6603.mk)
 $(call inherit-product, device/sony/common/radio.mk)
 $(call inherit-product, vendor/omni/config/gsm.mk)
 
-# Inherit Omni product configuration
-$(call inherit-product, vendor/omni/config/common.mk)
-
 # Override Product Name for OmniROM
 PRODUCT_NAME := omni_leo
 PRODUCT_MODEL := Xperia Z3
